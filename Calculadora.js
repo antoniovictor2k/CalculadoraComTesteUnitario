652cd7e6bd025c26607168ed37d8a4d385e1fdf7
Antonio Victor <avps2@aluno.ifal.edu.br>
2023-04-28 16:42:50 -0300

    fatorial ok

--- Calculadora.js
+++ Calculadora.js
@@ -4,68 +4,89 @@ var resultadoDoCalculo = document.getElementById('resultadoDoCalculo');
 
 
 function soma(a, b) {
-    var a = Number( numero1.value);
-    var b = Number( numero2.value);
-    console.log(a+b);
-    return resultadoDoCalculo.innerHTML= a + b 
-    
+    var a = Number(numero1.value);
+    var b = Number(numero2.value);
+    console.log(a + b);
+    return resultadoDoCalculo.innerHTML = a + b
+
 };
 
 function subtrair(a, b) {
-    var a = Number( numero1.value);
-    var b = Number( numero2.value);
-    return resultadoDoCalculo.innerHTML= a - b 
+    var a = Number(numero1.value);
+    var b = Number(numero2.value);
+    return resultadoDoCalculo.innerHTML = a - b
 
 };
-subtrair(5, 5);
 
 function multiplicar(a, b) {
-    var a = Number( numero1.value);
-    var b = Number( numero2.value);
-    return resultadoDoCalculo.innerHTML= a * b 
+    var a = Number(numero1.value);
+    var b = Number(numero2.value);
+    return resultadoDoCalculo.innerHTML = a * b
 
 };
-multiplicar(5, 5);
 
 function dividir(a, b) {
-    var a = Number( numero1.value);
-    var b = Number( numero2.value);
-  if (b == 0) {
-    return resultadoDoCalculo.innerHTML = 'Não é Possivel dividir Por 0'
-  }
-    return resultadoDoCalculo.innerHTML= a / b 
+    var a = Number(numero1.value);
+    var b = Number(numero2.value);
+    if (b == 0) {
+        return resultadoDoCalculo.innerHTML = 'Não é Possivel dividir Por 0'
+    }
+    return resultadoDoCalculo.innerHTML = a / b
 
 };
 
 function expoente(a, b) {
-    var a = Number( numero1.value);
-    var b = Number( numero2.value);
-    console.log(a**b);
-    return resultadoDoCalculo.innerHTML= a ** b 
+    var a = Number(numero1.value);
+    var b = Number(numero2.value);
+    console.log(a ** b);
+    return resultadoDoCalculo.innerHTML = a ** b
 
 };
 
 
-console.log('Antes da Impar ou Par');
 function verificarImparPar(a, b) {
-    var a = Number( numero1.value);
-    var b = Number( numero2.value);
-    a=5;
-    b=4;
-    var somaAB = a+b
+    var a = Number(numero1.value);
+    var b = Number(numero2.value);
+    var somaAB = a + b
     console.log('Durante a Impar ou Par')
-    if (somaAB %2==0) {
-        return console.log(somaAB +' é Par')
-    } 
-     else if(somaAB %2==1) {
-      
-        console.log("Else IF full")
+    if (somaAB % 2 == 0) {
+        return resultadoDoCalculo.innerHTML = somaAB + ' é Par'
     }
-    else{
-        console.log('else full')
+    else if (somaAB % 2 == 1) {
+
+        return resultadoDoCalculo.innerHTML = somaAB + " é Impar"
     }
-    // return console.log(a + b)
+    else {
+        return resultadoDoCalculo.innerHTML = somaAB + ' é Invalido ou não se encaixa'
+    }
+
 
 };
- verificarImparPar(5, 5);
-console.log('Depois da Impar ou Par');
+
+function raizquadrada(a, b) {
+    var a = Number(numero1.value);
+    var b = Number(numero2.value);
+    var somaAB = a + b;
+    var raiz = Math.sqrt(somaAB)
+    console.log('Durante a Impar ou Par')
+    return resultadoDoCalculo.innerHTML = raiz;
+
+};
+
+function fatorial(a, b) {
+    var a = Number(numero1.value);
+    var b = Number(numero2.value);
+
+    const calFatorial = n => {
+
+        if (n > 1) {
+            return n * calFatorial(n - 1)
+        }
+        return n
+    }
+
+    console.log(calFatorial(a + b))
+
+};
+
+
