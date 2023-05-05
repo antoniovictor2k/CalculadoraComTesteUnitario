296c272b82fa282d8dae52f24e2d0fbd2f0497c7
Antonio <avps2@aluno.ifal.edu.br>
2023-05-05 17:51:09 -0300

    add

--- Calculadora.js
+++ Calculadora.js
@@ -45,8 +45,7 @@ function dividir(a, b) {
 function expoente(a, b) {
     var a = Number(numero1.value);
     var b = Number(numero2.value);
-    console.log(a ** b);
-    return resultadoDoCalculo.innerHTML = a ** b
+    return resultadoDoCalculo.innerHTML = (a) ** b
 
 };
 
